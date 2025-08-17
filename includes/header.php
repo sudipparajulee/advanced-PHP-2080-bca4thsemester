8e3e31bb1d1c39bb67dd0ad374667833de177685
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-08-17 06:50:14 +0545

    first commit

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <nav class="flex items-center justify-between px-20 py-1 bg-yellow-600 text-white">
+        <img src="assets/logo.png" alt="" class="h-12">
+        <ul class="flex gap-3">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+    </nav>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include 'includes/header.php'; ?>
+
+    <h1>Welcome to BCA 4<sup>th</sup> Semester</h1>
+
+<?php include 'includes/footer.php'; ?>
