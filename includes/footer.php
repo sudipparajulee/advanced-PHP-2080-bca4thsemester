d10fdda3cb2b65b914fa524479e7d6df1bfab412
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-08-18 06:28:23 +0545

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,20 @@
+<footer>
+    <div class="bg-gray-100 py-6 px-20 grid grid-cols-3 gap-4">
+        <div>
+            <h2 class="text-lg font-bold">About Us</h2>
+            <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ratione tempore autem, explicabo facilis molestias laudantium magnam beatae odit blanditiis.</p>
+        </div>
+        <div class="pl-8">
+            <h2 class="text-lg font-bold">Quick Links</h2>
+            <div class="grid gap-1">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+                <a href="">Login</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
 </body>
 </html>
