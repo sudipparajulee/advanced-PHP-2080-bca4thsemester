ed4df3191a403a89c7e7f45134002b1238dbc539
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-08-18 06:45:05 +0545

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -13,6 +13,16 @@
                 <a href="">Login</a>
             </div>
         </div>
+        <div>
+            <h2 class="text-lg font-bold">Available At</h2>
+            <div class="flex gap-4">
+                <img src="assets/appstore.png" alt="">
+                <img src="assets/playstore.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center py-4 bg-gray-200">
+        <p class="text-sm text-gray-600">&copy; 2020-<?php echo date('Y') ?> Your Company. All rights reserved.</p>
     </div>
 </footer>
 
