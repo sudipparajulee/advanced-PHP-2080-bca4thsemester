cd6ed81eb753286339af3eaff2668d0352457a46
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-11-04 06:45:48 +0545

    Latest Product UI

--- index.php
+++ index.php
@@ -1,5 +1,49 @@
 <?php include 'includes/header.php'; ?>
 
-    <h1>Welcome to BCA 4<sup>th</sup> Semester</h1>
+    <div class="px-20 py-5">
+        <h1 class="font-bold text-3xl border-l-4 border-yellow-600 pl-2">Latest Products</h1>
+        <div class="grid grid-cols-5 gap-5">
+            <a href="" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
+                <img src="https://picsum.photos/200" alt="" class="w-full rounded-lg">
+                <h2 class="font-bold mt-2">Product Name</h2>
+                <p class="text-yellow-600 font-bold">Rs. 100 
+                    <span class="font-normal text-red-500 text-sm line-through">Rs. 120</span>
+                </p>
+                <span class="absolute right-0 top-0 text-xs bg-yellow-600 text-white px-1">20% Off</span>
+            </a>
+            <a href="" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
+                <img src="https://picsum.photos/201" alt="" class="w-full rounded-lg">
+                <h2 class="font-bold mt-2">Product Name</h2>
+                <p class="text-yellow-600 font-bold">Rs. 100 
+                    <span class="font-normal text-red-500 text-sm line-through">Rs. 120</span>
+                </p>
+                <span class="absolute right-0 top-0 text-xs bg-yellow-600 text-white px-1">20% Off</span>
+            </a>
+            <a href="" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
+                <img src="https://picsum.photos/202" alt="" class="w-full rounded-lg">
+                <h2 class="font-bold mt-2">Product Name</h2>
+                <p class="text-yellow-600 font-bold">Rs. 100 
+                    <span class="font-normal text-red-500 text-sm line-through">Rs. 120</span>
+                </p>
+                <span class="absolute right-0 top-0 text-xs bg-yellow-600 text-white px-1">20% Off</span>
+            </a>
+            <a href="" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
+                <img src="https://picsum.photos/203" alt="" class="w-full rounded-lg">
+                <h2 class="font-bold mt-2">Product Name</h2>
+                <p class="text-yellow-600 font-bold">Rs. 100 
+                    <span class="font-normal text-red-500 text-sm line-through">Rs. 120</span>
+                </p>
+                <span class="absolute right-0 top-0 text-xs bg-yellow-600 text-white px-1">20% Off</span>
+            </a>
+            <a href="" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
+                <img src="https://picsum.photos/204" alt="" class="w-full rounded-lg">
+                <h2 class="font-bold mt-2">Product Name</h2>
+                <p class="text-yellow-600 font-bold">Rs. 100 
+                    <span class="font-normal text-red-500 text-sm line-through">Rs. 120</span>
+                </p>
+                <span class="absolute right-0 top-0 text-xs bg-yellow-600 text-white px-1">20% Off</span>
+            </a>
+        </div>
+    </div>
 
 <?php include 'includes/footer.php'; ?>
