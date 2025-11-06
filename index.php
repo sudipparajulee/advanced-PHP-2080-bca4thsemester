849f7de05d990784efca23eddb838a8324de0785
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-11-06 06:45:42 +0545

    Product View

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <div class="px-20 py-5">
         <h1 class="font-bold text-3xl border-l-4 border-yellow-600 pl-2">Latest Products</h1>
         <div class="grid grid-cols-5 gap-5">
-            <a href="" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
+            <a href="viewproduct.php" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
                 <img src="https://picsum.photos/200" alt="" class="w-full rounded-lg">
                 <h2 class="font-bold mt-2">Product Name</h2>
                 <p class="text-yellow-600 font-bold">Rs. 100 
@@ -11,7 +11,7 @@
                 </p>
                 <span class="absolute right-0 top-0 text-xs bg-yellow-600 text-white px-1">20% Off</span>
             </a>
-            <a href="" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
+            <a href="viewproduct.php" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
                 <img src="https://picsum.photos/201" alt="" class="w-full rounded-lg">
                 <h2 class="font-bold mt-2">Product Name</h2>
                 <p class="text-yellow-600 font-bold">Rs. 100 
@@ -19,7 +19,7 @@
                 </p>
                 <span class="absolute right-0 top-0 text-xs bg-yellow-600 text-white px-1">20% Off</span>
             </a>
-            <a href="" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
+            <a href="viewproduct.php" class="shadow p-2 rounded-lg hover:shadow-lg relative hover:-translate-y-1 transition-all duration-200">
                 <img src="https://picsum.photos/202" alt="" class="w-full rounded-lg">
                 <h2 class="font-bold mt-2">Product Name</h2>
                 <p class="text-yellow-600 font-bold">Rs. 100 
---
+++ viewproduct.php
@@ -0,0 +1,17 @@
+<?php include 'includes/header.php'; ?>
+    
+    <div class="px-20 py-5 flex gap-5">
+        <div class="w-1/3">
+            <img src="https://picsum.photos/400" alt="" class="w-full rounded-lg">
+        </div>
+        <div class="w-2/3">
+            <h1 class="font-bold text-3xl">Product Name</h1>
+            <p class="text-yellow-600 font-bold text-2xl mt-2">Rs. 100 
+                <span class="font-normal text-red-500 text-sm line-through">Rs. 120</span>
+            </p>
+            <span class="text-xs bg-yellow-600 text-white px-1">20% Off</span>
+            <p class="mt-4">In Stock: 5</p>
+            <button class="bg-yellow-600 text-white py-2 rounded-lg px-4 mt-4">Add to Cart</button>
+        </div>
+    </div>
+<?php include 'includes/footer.php'; ?>
