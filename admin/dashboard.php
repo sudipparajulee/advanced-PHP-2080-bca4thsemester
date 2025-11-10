0fa082607d8d63d4e903e7da7b4b218d8bc17c40
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-11-10 06:45:11 +0545

    Dashboard

---
+++ admin/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="flex">
+        <div class="h-screen w-52 bg-gradient-to-r from-blue-600 to-blue-400 pt-10">
+            <a href="" class="block py-3 px-4 text-xl text-white hover:bg-white hover:text-blue-600">Dashboard</a>
+            <a href="" class="block py-3 px-4 text-xl text-white hover:bg-white hover:text-blue-600">Categories</a>
+            <a href="" class="block py-3 px-4 text-xl text-white hover:bg-white hover:text-blue-600">Products</a>
+            <a href="" class="block py-3 px-4 text-xl text-white hover:bg-white hover:text-blue-600">Users</a>
+            <a href="" class="block py-3 px-4 text-xl text-white hover:bg-white hover:text-blue-600">Orders</a>
+            <a href="" class="block py-3 px-4 text-xl text-white hover:bg-white hover:text-blue-600">Logout</a>
+        </div>
+    </div>
+</body>
+</html>
