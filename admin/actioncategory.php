37b13499f3fa1dcc2ac4e1209c30bdb698cdef45
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-11-19 06:46:41 +0545

    updates

--- admin/actioncategory.php
+++ admin/actioncategory.php
@@ -5,13 +5,4 @@ $name = $_POST['name'];
 echo $priority;
 echo $name;
 
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "smc_bcaproject4th";
-
-$conn = mysqli_connect($server, $username, $password, $database);
-
-if(!$conn){
-    die("<script>alert('Connection Failed.')</script>");
-}
+include 'includes/dbconnection.php';
---
+++ admin/includes/dbconnection.php
@@ -0,0 +1,11 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "smc_bcaproject4th";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+if(!$conn){
+    die("<script>alert('Connection Failed.')</script>");
+}
